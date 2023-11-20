f8dec6cb239306d08c016e8c4d385ca87468e10b
matiasbiagetti <biagettimati@gmail.com>
2023-11-20 14:24:03 -0300

    deps and db

--- app/configs/database.py
+++ app/configs/database.py
@@ -7,14 +7,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
 
 # Get environment variables
-DB_DIALECT = os.getenv("DB_DIALECT", "mysql")
+DB_DIALECT = os.getenv("DB_DIALECT", "jdbc:postgresql")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "educacion")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Create database engine
 Engine = create_engine(DATABASE_URL, echo=True)
